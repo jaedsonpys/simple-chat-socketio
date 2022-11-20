6925bee4b363997960db94779415e97b1829060e
Jaedson Silva <contacanal798@gmail.com>
2022-11-20 19:20:31 -0300

    checking username before register

--- static/index.js
+++ static/index.js
@@ -1,11 +1,19 @@
 const sio = io();
 
+const userRegisterDiv = document.querySelector('#user-register');
 const usernameInput = document.querySelector('#username');
 const chatTextArea = document.querySelector('#chat');
 
 const setUsername = () => {
     const username = usernameInput.value;
-    sio.emit('set_username', username);
+
+    if(username.length !== 0) {
+        userRegisterDiv.style.display = 'none';
+        usernameInput.style.border = 'none';
+        sio.emit('set_username', username);
+    } else {
+        usernameInput.style.border = 'solid 1px red';
+    }
 }
 
 sio.on('connect', (socket) => {
