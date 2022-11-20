f8d6a72ce515ceabd4cd7013f68f43246611f1d8
Jaedson Silva <contacanal798@gmail.com>
2022-11-20 18:40:13 -0300

    adding event to broadcast client message

--- server.py
+++ server.py
@@ -19,5 +19,12 @@ async def set_username(sid, username):
     await sio.emit('new_user', data=username, skip_sid=sid)
 
 
+@sio.event
+async def send_message(sid, message):
+    if users.get(sid):
+        from_user = users[sid]
+        sio.emit('new_message', data={'from': from_user, 'message': message}, skip_sid=sid)
+
+
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=5500)
