6aeb305a5d8cccd30f3e0d4200c28e29798a50d7
Jaedson Silva <contacanal798@gmail.com>
2022-11-20 18:27:22 -0300

    adding connect event to register websocket id

--- server.py
+++ server.py
@@ -5,6 +5,13 @@ sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 
+users = {}
+
+
+@sio.event
+def connect(sid, environ):
+    users[sid] = None
+
 
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=5500)
